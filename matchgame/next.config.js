cc0d06841ae3576aa1e29f4d2877d2dc5252cfa4
Ali <iqalisaleh@gmail.com>
2022-03-13 22:05:49 +0300

    functionality in porgress

--- matchgame/next.config.js
+++ matchgame/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["assets.www.unsplash.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
--- matchgame/pages/index.js
+++ matchgame/pages/index.js
@@ -1,107 +1,78 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import logo from "../styles/2.png";
 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [pics, setPics] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const myLoader = ({ src }) => {};
+
+  ` https://unsplash.com/photos/SSxIGsySh8o`;
+  // console.log(pics);
+
+  function shufflePics() {
+    const shuffledPics = [...data, ...data]
+      .sort(() => Math.random() - 0.5)
+      .map((pic) => ({ ...pic, id: Math.random() }));
+    setPics(shuffledPics);
+  }
+
+  function clickHandler(card) {
+    choiceOne ? setChoiceOne(card) : setChoiceTwo(card);
+  }
+
+  console.log(pics);
+
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list?page=2&limit=8")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title> Matching Game</title>
       </Head>
-      
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-        Matching Game
-        </h1>
+        <button onClick={shufflePics}>test</button>
+        <h1 className={styles.title}>Matching Game</h1>
         <div className={styles.grid}>
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" layout="fill" />    
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>
-
-          <div className={styles.card}>
-            <Image className="front-face" src={logo} layout="fill" />
-            <img className="back-face" src="" />  
-          </div>         
+          {pics.map((pic) => {
+            return (
+              <div key={pic.id} className={styles.card}>
+                <div>
+                  <Image
+                    loader={myLoader}
+                    className="front-face"
+                    src={pic.download_url}
+                    layout="fill"
+                    alt="unflipped image"
+                  />
+                  <Image
+                    onClick={() => clickHandler(pic)}
+                    className="back-face"
+                    src="/test.ico"
+                    layout="fill"
+                    alt="flipped image"
+                  />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </main>
-      </div>
-    
-  )
+    </div>
+  );
 }
-
 
 /*
 const Card = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
