20976977a359671eac3405cc0ac177cb2ef20fd3
Ali <iqalisaleh@gmail.com>
2022-03-14 21:00:35 +0300

    finished

--- matchgame/next.config.js
+++ matchgame/next.config.js
@@ -3,7 +3,6 @@
 module.exports = nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.www.unsplash.com"],
-    formats: ["image/avif", "image/webp"],
+    domains: ["images.unsplash.com"],
   },
 };
